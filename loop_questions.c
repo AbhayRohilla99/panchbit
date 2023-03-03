054330db53d743f373918ffd92809e5a69b5619c
AbhayRohilla99 <126755671+AbhayRohilla99@users.noreply.github.com>
2023-03-03 22:09:56 +0530

    AbhayRohilla99 Update This file

--- loop_questions.c
+++ loop_questions.c
@@ -3,8 +3,8 @@
 
 int main(){
    
-  printf("the first 10 natural odd numbers:");
-  for(int i=1;i<=10;i++){
+  printf("the first 10 natural odd numbers in reverse order:");
+  for(int i=10;i>=1;i--){
       printf("%d\n",2*i-1);}
 
         
