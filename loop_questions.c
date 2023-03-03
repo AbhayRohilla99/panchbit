2f99aa3fc3b4527fc6fe10b8c02f219d55eaff34
AbhayRohilla99 <126755671+AbhayRohilla99@users.noreply.github.com>
2023-03-03 22:05:41 +0530

    Create new file

---
+++ loop_questions.c
@@ -0,0 +1,15 @@
+
+#include <stdio.h>
+
+int main(){
+   
+  printf("the first 10 natural odd numbers:");
+  for(int i=1;i<=10;i++){
+      printf("%d\n",2*i-1);}
+
+        
+    }
+    
+    
+    
+    
