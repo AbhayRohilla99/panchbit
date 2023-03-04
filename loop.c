6bcff2a65f82883431eeef6091a0e3e757b4bbf1
AbhayRohilla99 <126755671+AbhayRohilla99@users.noreply.github.com>
2023-03-04 19:58:35 +0530

    Create new file

---
+++ loop.c
@@ -0,0 +1,14 @@
+
+#include <stdio.h>
+
+int main(){
+    int i,n;
+    printf("enter the value of n");
+    scanf("%d",&n);
+    printf("printing natural number from 1 to %d\n",n);
+    for(i=1;i<=n;i++){
+        printf("%d\n",i);
+    }
+    
+    
+}
