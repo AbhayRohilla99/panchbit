638476f98160369aa5e34e183fe2f25c5060726b
AbhayRohilla99 <126755671+AbhayRohilla99@users.noreply.github.com>
2023-03-06 21:02:04 +0530

    AbhayRohilla99 Update This file

--- inverted_full_pyramid.c
+++ inverted_full_pyramid.c
@@ -3,8 +3,8 @@
 
 int main(){
     int i,j,k;
-    for(i=5;i>=1;i--){
-    for(j=5;j>i;j--)
+    for(i=4;i>=1;i--){
+    for(j=7;j>i;j--)
     printf(" ");
     for(k=1;k<=(i*2)-1;k++)
         printf("*");
