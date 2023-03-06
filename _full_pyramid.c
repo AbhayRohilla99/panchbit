0ac2e852b751d85bbdeb042fac6329f3d1deae91
AbhayRohilla99 <126755671+AbhayRohilla99@users.noreply.github.com>
2023-03-06 21:00:31 +0530

    Create new file

---
+++ _full_pyramid.c
@@ -0,0 +1,16 @@
+
+#include <stdio.h>
+
+int main(){
+    int i,j,k;
+    for(i=1;i<=4;i++){
+    for(j=7;j>=i;j--)
+    printf(" ");
+    for(k=1;k<=(i*2)-1;k++)
+        printf("*");
+        printf("\n");}
+    }
+    
+    
+    
+    
