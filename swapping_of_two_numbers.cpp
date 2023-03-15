04f48a52cde0244fee2fc50511482d1f163612de
AbhayRohilla99 <126755671+AbhayRohilla99@users.noreply.github.com>
2023-03-15 22:44:18 +0530

    Create new file

---
+++ swapping_of_two_numbers.cpp
@@ -0,0 +1,19 @@
+
+#include <iostream>
+using namespace std;
+
+int main(){
+    int a,b,temp;
+    cout<<"a";
+    cin>>a;
+    cout<<"b";
+    cin>>b;
+    temp=a;
+    a=b;
+    b=temp;
+    cout<<"swapping the value of a and b="<<a<<b;
+    
+    
+    
+    
+}
