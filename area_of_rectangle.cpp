8a7cab7845f0151732f097fd18b0d2a6018cebf6
AbhayRohilla99 <126755671+AbhayRohilla99@users.noreply.github.com>
2023-03-16 21:25:08 +0530

    Create new file

---
+++ area_of_rectangle.cpp
@@ -0,0 +1,16 @@
+
+#include <iostream>
+using namespace std;
+
+int main(){
+    int l,b,area;
+    cout<<"l";
+    cin>>l;
+    cout<<"b";
+    cin>>b;
+    area=l*b;
+    cout<<"area of rectangle is"<<endl<<area;
+    
+    
+    
+}
