03e812dc9dbc4e915e7d82452af18da5202dcd3d
AbhayRohilla99 <126755671+AbhayRohilla99@users.noreply.github.com>
2023-03-17 22:02:32 +0530

    Create new file

---
+++ WAP_design_a_calculator_to_perform_basic_arithmetic_operation.cpp
@@ -0,0 +1,19 @@
+
+#include <iostream>
+using namespace std;
+
+int main(){
+    float num1,num2;
+    cout<<"enter the first number";
+    cin>>num1;
+    cout<<"enter the second number";
+    cin>>num2;
+    cout<<"Sum="<<num1+num2<<endl;
+    cout<<"Difference="<<num1-num2<<endl;
+    cout<<"Product="<<num1*num2<<endl;
+    cout<<"Divison="<<num1/num2<<endl;
+    
+    
+    
+    
+}
