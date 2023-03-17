bb28a88f2eb41649561af0cd35c16211260073b9
AbhayRohilla99 <126755671+AbhayRohilla99@users.noreply.github.com>
2023-03-17 21:24:53 +0530

    Create new file

---
+++ find_grade_.cpp
@@ -0,0 +1,15 @@
+
+#include <iostream>
+using namespace std;
+
+int main(){
+    int total,boys,girls;
+    boys=17;
+    total=(80*45)/100;
+    girls=total-boys;
+    cout<<"number of girls getting grade A="<<girls<<endl;
+    
+    
+    
+    
+}
