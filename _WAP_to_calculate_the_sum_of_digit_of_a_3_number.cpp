e2d573dae2f28196a010095a3bff6de0d65ab99f
AbhayRohilla99 <126755671+AbhayRohilla99@users.noreply.github.com>
2023-03-17 21:56:21 +0530

    Create new file

---
+++ _WAP_to_calculate_the_sum_of_digit_of_a_3_number.cpp
@@ -0,0 +1,20 @@
+
+#include <iostream>
+using namespace std;
+
+int main(){
+    int n,first,second,third,sum;
+    n=326;
+    first=n/100;
+    n=n%100;
+    second=n/10;
+    n=n%10;
+    third=n%10;
+    sum=first+second+third;
+    cout<<"sum of 3 digit is="<<sum<<endl;
+    
+    
+    
+    
+    
+}
