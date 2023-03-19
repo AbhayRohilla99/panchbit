64869cde2a873a4b35eb65f792c68c370e16351c
AbhayRohilla99 <126755671+AbhayRohilla99@users.noreply.github.com>
2023-03-19 22:03:30 +0530

    Create new file

---
+++ WAP_to_print_absolute_value_of_a_number__entered_by_user_.cpp
@@ -0,0 +1,18 @@
+
+#include <iostream>
+using namespace std;
+
+int main(){
+    int x;
+   cout<<"enter a number="<<endl;
+   cin>>x;
+   if(x<0){
+       x= x*(-1);}
+       cout<<"absolute value is="<<x<<endl;
+       
+       }
+    
+    
+    
+    
+    
