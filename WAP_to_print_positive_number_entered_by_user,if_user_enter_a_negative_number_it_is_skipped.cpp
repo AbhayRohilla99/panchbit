64e9a85901b908d3d60ea1d31c2c56827f3877c9
AbhayRohilla99 <126755671+AbhayRohilla99@users.noreply.github.com>
2023-03-19 22:14:34 +0530

    Create new file

---
+++ WAP_to_print_positive_number_entered_by_user,if_user_enter_a_negative_number_it_is_skipped.cpp
@@ -0,0 +1,18 @@
+
+#include <iostream>
+using namespace std;
+
+int main(){
+    int number;
+    cout<<"enter a number:"<<endl;
+    cin>>number;
+    if(number>0){
+        cout<<"you entered a positive integer:"<<number<<endl;}
+        else{
+            cout<<"the number is negative and skipped:";
+        }
+    }
+    
+    
+    
+    
