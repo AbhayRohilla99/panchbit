215a175014577d673fd8df38360c1670379df073
AbhayRohilla99 <126755671+AbhayRohilla99@users.noreply.github.com>
2023-04-04 20:51:10 +0530

    Create new file

---
+++ calculate_the_sum_of_all_the_elements_in_the_given_array.cpp
@@ -0,0 +1,17 @@
+
+#include <iostream>
+using namespace std;
+
+int main(){
+    int arr[]={3,6,11,10,10};
+    int sum=0;
+    for(int i=0;i<5;i++){
+        sum=sum+arr[i];
+    }
+    cout<<sum<<endl;
+    
+    
+    
+    
+    
+}
