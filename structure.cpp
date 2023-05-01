16b3d000a532a857cc5d639f56a5e56a6de6cda7
AbhayRohilla99 <126755671+AbhayRohilla99@users.noreply.github.com>
2023-05-01 22:23:21 +0530

    Create new file

---
+++ structure.cpp
@@ -0,0 +1,18 @@
+
+#include <iostream>
+#include<cstring>
+using namespace std;
+ struct book{
+     int book ;
+     char tittle[20];
+     float price;
+ };
+ 
+    int  main(){
+        book b1={100,"c++ by saurabh",450.0}; 
+        book b2,b3;
+        strcpy(b2.tittle,"c++ made easy");
+        b2.price=300.0;
+        b2=b3;}
+    
+    
