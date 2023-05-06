59b0c2120d9b1935542b0437dd3bcc0cb8a4df01
AbhayRohilla99 <126755671+AbhayRohilla99@users.noreply.github.com>
2023-05-06 07:22:50 +0530

    Create new file

---
+++ constructor.cpp
@@ -0,0 +1,19 @@
+
+#include <iostream>
+using namespace std;
+
+class complex{
+    private:
+    int a,b;
+    public:
+    complex()
+    {cout<<"hello constructor";}
+    };
+
+    int main(){
+        complex c1;
+    }
+    
+    
+    
+    
