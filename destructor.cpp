726fd95bb813af7a5893da8e75d927204c78b2e7
AbhayRohilla99 <126755671+AbhayRohilla99@users.noreply.github.com>
2023-05-07 12:39:52 +0530

    Create new file

---
+++ destructor.cpp
@@ -0,0 +1,22 @@
+
+#include <iostream>
+using namespace std;
+
+class Complex
+{
+    private:
+    int a,b;
+    public:
+    ~Complex()   
+     {cout<<"destructor";} 
+        };
+
+
+    void fun(){
+        Complex obj;
+    }
+    int main(){
+        fun();
+    }
+    
+    
