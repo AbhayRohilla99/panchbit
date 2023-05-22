457dd773e454cad3bfd2067fe46ee8dbb43d8fd2
AbhayRohilla99 <126755671+AbhayRohilla99@users.noreply.github.com>
2023-05-22 08:18:11 +0530

    Create new file

---
+++ class_type_to_another_class_type.cpp
@@ -0,0 +1,34 @@
+
+#include <iostream>
+using namespace std;
+
+class product{
+    private:
+    int m,n;
+    public:
+    void setData(int x,int y)
+    {m=x;n=y;}
+    int getM()
+    {return(m);}
+    int getN()
+    {return(n);}
+    
+};
+class item{
+    private:
+    int a,b;
+    public:
+    void showData()
+    {cout<<"a="<<a<<"b="<<b;}
+    item() {  };
+    item(product p){
+        a=p.getM();
+        b=p.getN();}
+    };
+    int main(){
+        item i1;
+        product p1;
+        p1.setData(3,4);
+        i1=p1;
+        i1.showData();
+    }
