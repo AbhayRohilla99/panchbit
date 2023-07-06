87ab8b4748c9dd8421a504fdb3f7765a623e9404
AbhayRohilla99 <126755671+AbhayRohilla99@users.noreply.github.com>
2023-07-06 22:05:12 +0530

    Create new file

---
+++ palindrome___by_recursion.cpp
@@ -0,0 +1,23 @@
+
+#include <iostream>
+using namespace std;
+
+bool checkPalindrome(string str,int i,int j){
+    if(i>j)
+    return true;
+    if(str[i] !=str[j])
+    return false;
+  else{
+      return checkPalindrome(str,i+1,j-1);
+  }
+    
+}
+int main(){
+    string name="abba";
+    cout<<endl;
+    bool isPalindrome=checkPalindrome(name,0,name.length()-1);
+    if(isPalindrome)
+    cout<<"it is palindrome"<<endl;
+    else
+    cout<<"it is not palindrome";
+}
