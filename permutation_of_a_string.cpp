1d20ec3fd3fc8ba52da3fc64c513fd8212180e30
AbhayRohilla99 <126755671+AbhayRohilla99@users.noreply.github.com>
2023-07-08 22:15:46 +0530

    Create new file

---
+++ permutation_of_a_string.cpp
@@ -0,0 +1,22 @@
+
+#include <iostream>
+using namespace std;
+void permute(string& a,int l,int r){
+    if(l==r)
+        cout<<a<<endl;
+        else{
+            for(int i=l;i<=r;i++){
+                swap(a[l],a[i]);
+                //recusion called
+                permute(a,l+1,r);
+                //backtracking
+                swap(a[l],a[i]);
+            }
+        }
+    }
+int main(){
+    string str="ABC";
+    int n=str.size();
+    permute(str,0,n-1);
+    return 0;
+}
